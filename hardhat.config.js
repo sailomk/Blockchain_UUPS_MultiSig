d36a4a00d18270cd48d0787b37819dfdb4026cd3
ZoftBit <sailomk.@gmail.com>
2025-08-18 18:30:27 +0700

    Fronend 100% Done

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,7 @@ module.exports = {
 			// This sets the block gas limit for the Hardhat Network
 			// Default is usually 30,000,000. If your transaction is very large,
 			// you might need to increase it, but 10M for a single tx is already high.
-			blockGasLimit: 30_000_000, // Example: Increase if needed, but 30M is standard
+			//blockGasLimit: 30_000_000, // Example: Increase if needed, but 30M is standard
 		},
 	},
 };
